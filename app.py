3bf1d368d21623d0730b7dfc87a88ea79634733a
Shivashis Adhikari <shivashisadhikari@ratobangala.edu.np>
2025-04-24 15:18:08 +0545

    List order
    Fixed list order to list all items alphabetically

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 sheet_id = "14TPNWMPCNZi4DIhqUTOFJYHZShPuuQfodWDhE2-0I8Y"
 sheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
 
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+# gsheets URL
+sheet_id = "14TPNWMPCNZi4DIhqUTOFJYHZShPuuQfodWDhE2-0I8Y"
+sheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+
 @app.route('/')
 def index():
     df = pd.read_csv(sheet_url)
@@ -19,8 +28,14 @@ def index():
         else:
             df = df[df[search_field].astype(str).str.contains(search_query, case=False)]
 
+    df = df.dropna(subset=['Title'])
+    df = df.sort_values(by='Title', key=lambda col: col.str.lower())
+
     books = df.to_dict(orient='records')
     return render_template('index.html', books=books, query=search_query, field=search_field)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
